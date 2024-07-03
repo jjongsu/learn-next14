dde73de17af8120daeab559ca0cdff57300013ac
jongsu <hjs1002@jei.com>
2024-07-03 15:14:08 +0900

    💄 Style : css

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
+import styles from '../styles/navigation.module.css';
 
 export default function Navigation() {
     const path = usePathname();
@@ -10,7 +11,7 @@ export default function Navigation() {
     const [count, setCount] = useState(0);
 
     return (
-        <nav>
+        <nav className={styles.nav}>
             <ul>
                 <li>
                     <Link href={'/'}>Home</Link>
