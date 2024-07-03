575db4401fdd3bf4b1e1c24be822f9832113580a
jongsu <hjs1002@jei.com>
2024-07-03 14:55:54 +0900

    ✨ Feat : error page

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -3,8 +3,9 @@ import { API_URL } from '../app/(home)/page';
 const getVideos = async (id: string) => {
     console.log(`fetching videos : ${Date.now()}`);
     await new Promise(res => setTimeout(res, 5000));
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    return response.json();
+    throw new Error('something broke!!');
+    // const response = await fetch(`${API_URL}/${id}/videos`);
+    // return response.json();
 };
 
 export default async function MovieVideos({ id }: { id: string }) {
