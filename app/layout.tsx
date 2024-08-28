ee0d35251fbaecd582f913b8d5a7796b325cc432
황종수 <jongsuhwang@hansol.com>
2024-08-28 12:59:25 +0900

    📈 Add : hotjar javascript code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '../styles/global.css';
 import { Metadata } from 'next';
 import Navigation from '../components/navigation';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
     // title: 'Home | Next Movies',
@@ -17,6 +18,19 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en'>
+            <Script
+                id='hotjar-javascript'
+                dangerouslySetInnerHTML={{
+                    __html: `(function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:5111622,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+                }}
+            />
             <body>
                 <Navigation />
                 {children}
